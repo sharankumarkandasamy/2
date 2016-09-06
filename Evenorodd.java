e043497f18368e2ef30c114f9a412ffb16a5a9f9
sharankumarkandasamy <sharankumar29.2@gmail.com>
2016-09-06 21:02:49 +0530

    Add files via upload

---
+++ Evenorodd.java
@@ -0,0 +1,22 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Evenorodd 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int n;
+		System.out.println("Enter the number");
+		n=sc.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+}
+}
